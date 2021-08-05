8e7d2df0495dc78be764ab3cba0672a5c2c1c65c
William Liu <williamylliu@gmail.com>
2021-08-05 03:34:41 +0000

    learn lambda

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImp.java
@@ -1,12 +1,15 @@
 package ca.jrvs.apps.practice;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public interface LambdaStreamExc {
+public class LambdaStreamExcImp implements LambdaStreamExc {
 
   /**
    * Create a String stream from array
@@ -16,8 +19,10 @@ public interface LambdaStreamExc {
    * @param strings
    * @return
    */
-  Stream<String> createStrStream(String... strings);
-
+  @Override
+  public Stream<String> createStrStream(String... strings) {
+    return Arrays.stream(strings);
+  }
 
   /**
    * Convert all strings to uppercase
@@ -26,7 +31,10 @@ public interface LambdaStreamExc {
    * @param strings
    * @return
    */
-  Stream<String> toUpperCase(String... strings);
+  @Override
+  public Stream<String> toUpperCase(String... strings) {
+    return createStrStream(strings).map(String::toUpperCase);
+  }
 
   /**
    * filter strings that contains the pattern
@@ -36,7 +44,10 @@ public interface LambdaStreamExc {
    * @param pattern
    * @return
    */
-  Stream<String> filter(Stream<String> stringStream, String pattern);
+  @Override
+  public Stream<String> filter(Stream<String> stringStream, String pattern) {
+    return stringStream.filter(str -> !str.contains(pattern));
+  }
 
   /**
    * Create a intStream from a arr[]
@@ -44,16 +55,21 @@ public interface LambdaStreamExc {
    * @param arr
    * @return
    */
-  IntStream createIntStream(int[] arr);
+  @Override
+  public IntStream createIntStream(int[] arr) {
+    return Arrays.stream(arr);
+  }
 
   /**
    * Convert a stream to list
    *
    * @param stream
-   * @param <E>
    * @return
    */
-  <E> List<E> toList(Stream<E> stream);
+  @Override
+  public <E> List<E> toList(Stream<E> stream) {
+    return stream.collect(Collectors.toList());
+  }
 
   /**
    * Convert a intStream to list
@@ -61,7 +77,10 @@ public interface LambdaStreamExc {
    * @param intStream
    * @return
    */
-  List<Integer> toList(IntStream intStream);
+  @Override
+  public List<Integer> toList(IntStream intStream) {
+    return intStream.boxed().collect(Collectors.toList());
+  }
 
   /**
    * Create a IntStream range from start to end inclusive
@@ -70,7 +89,10 @@ public interface LambdaStreamExc {
    * @param end
    * @return
    */
-  IntStream createIntStream(int start, int end);
+  @Override
+  public IntStream createIntStream(int start, int end) {
+    return IntStream.rangeClosed(start, end);
+  }
 
   /**
    * Convert a intStream to a doubleStream and compute square root of each element
@@ -78,8 +100,10 @@ public interface LambdaStreamExc {
    * @param intStream
    * @return
    */
-  DoubleStream squareRootIntStream(IntStream intStream);
-
+  @Override
+  public DoubleStream squareRootIntStream(IntStream intStream) {
+    return intStream.asDoubleStream().map(Math::sqrt);
+  }
 
   /**
    * filter all even number and return odd numbers from a intStream
@@ -87,7 +111,10 @@ public interface LambdaStreamExc {
    * @param intStream
    * @return
    */
-  IntStream getOdd(IntStream intStream);
+  @Override
+  public IntStream getOdd(IntStream intStream) {
+    return intStream.filter(i -> i % 2 == 1);
+  }
 
   /**
    * Return a lambda function that print a message with a prefix and suffix
@@ -108,7 +135,10 @@ public interface LambdaStreamExc {
    * @param suffix suffix str
    * @return
    */
-  Consumer<String> getLambdaPrinter(String prefix, String suffix);
+  @Override
+  public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
+    return msg -> System.out.println(prefix + msg + suffix);
+  }
 
   /**
    * Print each message with a given printer
@@ -126,7 +156,10 @@ public interface LambdaStreamExc {
    * @param messages
    * @param printer
    */
-  void printMessages(String[] messages, Consumer<String> printer);
+  @Override
+  public void printMessages(String[] messages, Consumer<String> printer) {
+    Arrays.stream(messages).forEach(printer);
+  }
 
   /**
    * Print all odd number from a intStream.
@@ -142,7 +175,10 @@ public interface LambdaStreamExc {
    * @param intStream
    * @param printer
    */
-  void printOdd(IntStream intStream, Consumer<String> printer);
+  @Override
+  public void printOdd(IntStream intStream, Consumer<String> printer) {
+    getOdd(intStream).forEach(i -> printer.accept(String.valueOf(i)));
+  }
 
   /**
    * Square each number from the input.
@@ -152,5 +188,8 @@ public interface LambdaStreamExc {
    * @param ints
    * @return
    */
-  Stream<Integer> flatNestedInt(Stream<List<Integer>> ints);
+  @Override
+  public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
+    return ints.flatMap(Collection::stream).map(i -> i * i);
+  }
 }
